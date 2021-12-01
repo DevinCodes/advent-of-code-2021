7171a9a15b6e34a3eb0f42895a8377383ef87174
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2021-12-01 18:12:15 +0100

    feat: day 2

---
+++ day-2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	advent_of_code "advent-of-code"
+	"fmt"
+)
+
+func main() {
+	numbers := advent_of_code.NumbersListFromInput()
+	sums := make([]int, 0)
+	var currSum int
+	maxIndex := len(numbers) - 1
+
+	for i, number := range numbers {
+		if i > maxIndex - 2 {
+			break
+		}
+
+		currSum += number
+		currSum += numbers[i+1]
+		currSum += numbers[i+2]
+
+		sums = append(sums, currSum)
+		currSum = 0
+	}
+
+	increases := advent_of_code.NumberOfIncreases(sums)
+	fmt.Printf("Number of increases: %d\n", increases)
+
+}
