17fa91e84f2e4a0dc0a493816fa02aaab479df5f
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2021-12-01 17:45:11 +0100

    feat: day 1

---
+++ day-1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	values := make([]int, 0)
+	count := 0
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		index := len(values)
+		values = append(values, num)
+		if index < 1 {
+			continue
+		}
+
+		prevValue := values[index-1]
+
+		if prevValue < num {
+			count++
+		}
+	}
+
+	fmt.Printf("Number of increases: %d\n", count)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
