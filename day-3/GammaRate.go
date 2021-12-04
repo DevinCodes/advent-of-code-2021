539b46b6e7bcf3ed40faf3b692215ed46c7f92a1
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2021-12-04 12:30:40 +0100

    chore: refactor

---
+++ day-3/GammaRate.go
@@ -0,0 +1,26 @@
+package day_3
+
+func GammaRate(lines []string) []string {
+	length := len(lines[0])
+	numLines := len(lines)
+	threshold := numLines / 2
+
+	var numZeros int
+	var gammaRate []string
+	for i := 0; i < length; i++ {
+		numZeros = 0
+		for _, line := range lines {
+			if string(line[i]) == "0" {
+				numZeros++
+			}
+		}
+
+		if numZeros > threshold {
+			gammaRate = append(gammaRate, "0")
+		} else {
+			gammaRate = append(gammaRate, "1")
+		}
+	}
+
+	return gammaRate
+}
--- day-3/part-1/main.go
+++ day-3/part-1/main.go
@@ -10,31 +10,8 @@ import (
 func main() {
 	lines := day_3.LinesFromInput()
 
-	length := len(lines[0])
-	numLines := len(lines)
-	threshold := numLines / 2
-
-	fmt.Printf("Length is %d\n", length)
-
-	var numZeros int
-	var gammaRate []string
-	var epsilonRate []string
-	for i := 0; i < length; i++ {
-		numZeros = 0
-		for _, line := range lines {
-			if string(line[i]) == "0" {
-				numZeros++
-			}
-		}
-
-		if numZeros > threshold {
-			gammaRate = append(gammaRate, "0")
-			epsilonRate = append(epsilonRate, "1")
-		} else {
-			gammaRate = append(gammaRate, "1")
-			epsilonRate = append(epsilonRate, "0")
-		}
-	}
+	gammaRate := day_3.GammaRate(lines)
+	epsilonRate := day_3.EpsilonRate(lines)
 
 	g, _ := strconv.ParseInt(strings.Join(gammaRate, ""), 2, 64)
 	e, _ := strconv.ParseInt(strings.Join(epsilonRate, ""), 2, 64)
